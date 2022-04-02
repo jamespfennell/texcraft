156dbc9cf7a11994f49ebd7464880163f5aa93d3
James Fennell <jamespfennell@gmail.com>
2022-04-02 16:24:40 -0400

    Use clap in the fttopl program

--- bin/src/tftopl.rs
+++ bin/src/tftopl.rs
@@ -1,6 +1,29 @@
+use clap::{ArgEnum, Parser};
+
+#[derive(Parser)]
+#[clap(version, name = "tftopl")]
+struct Cli {
+    /// Path to the TeX font metric (tfm) file
+    #[clap(parse(from_os_str))]
+    tfm_path: std::path::PathBuf,
+}
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let tfm_bytes = std::fs::read(&args[1]).unwrap();
+    let args = Cli::parse();
+    let tfm_path = if args.tfm_path.extension().is_none() {
+        let mut tfm_path = args.tfm_path;
+        tfm_path.set_extension("tfm");
+        tfm_path
+    } else {
+        args.tfm_path
+    };
+    let tfm_bytes = match std::fs::read(&tfm_path) {
+        Ok(contents) => contents,
+        Err(err) => {
+            println!("Failed to open {:?}: {}", tfm_path, err);
+            std::process::exit(1);
+        }
+    };
     let file = tfm::parse_tfm(&tfm_bytes);
     let pl_str = tfm::serialize_pl(&file, tfm::PlStyle::default());
     println!("{}", pl_str);
